f1652c6d0bc9ddf21bf2dc0ccb43186174e41497
LukasKrat <vy388@gmx.de>
2023-12-05 11:43:16 +0100

    changed usage-case;

--- Aufgabe2/f/main.c
+++ Aufgabe2/f/main.c
@@ -2,14 +2,14 @@
 #include "myDateFunctions.h"
 
 int main() {
-    int input[12] = {2000,1,1,14,05,0};
-    int input2[12] = {2001,1,1,14,05,0};
+    int input[12] = {2001,1,1,14,05,0};
+    int input2[12] = {2000,1,1,14,05,0};
     // printf("%lld\n", toUnixtime(input));
     // long long ts = toUnixtime(input);
     // long long ts2 = toUnixtime(input2);
     // printf("%lld\n", toUnixtime(input));
     // printf("%lld\n", ts);
     // printf("%s\n", unixtimeToString(ts));
-    printf("%d", compare(input, input2));
+    printf("%d\n", compare(input, input2));
     return 0;
 }
