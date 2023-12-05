f9754ac9f16a27e5be4ab388a0fad222e419553c
LukasKrat <vy388@gmx.de>
2023-12-05 11:09:57 +0100

    added uses;

--- Aufgabe2/f/main.c
+++ Aufgabe2/f/main.c
@@ -3,10 +3,13 @@
 
 int main() {
     int input[12] = {2000,1,1,14,05,0};
-    printf("%lld\n", toUnixtime(input));
-    long long ts = toUnixtime(input);
-    printf("%lld\n", toUnixtime(input));
-    printf("%lld\n", ts);
-    printf("%s\n", unixtimeToString(ts));
+    int input2[12] = {2001,1,1,14,05,0};
+    // printf("%lld\n", toUnixtime(input));
+    // long long ts = toUnixtime(input);
+    // long long ts2 = toUnixtime(input2);
+    // printf("%lld\n", toUnixtime(input));
+    // printf("%lld\n", ts);
+    // printf("%s\n", unixtimeToString(ts));
+    printf("%d", compare(input, input2));
     return 0;
 }
--- Aufgabe2/g/main.c
+++ Aufgabe2/g/main.c
@@ -3,10 +3,13 @@
 
 int main() {
     int input[12] = {2000,1,1,14,05,0};
-    printf("%lld\n", toUnixtime(input));
-    long long ts = toUnixtime(input);
-    printf("%lld\n", toUnixtime(input));
-    printf("%lld\n", ts);
-    printf("%s\n", unixtimeToString(ts));
+    int input2[12] = {1997,1,1,14,05,0};
+    int input3[12] = {2030,1,1,14,05,0};
+    // printf("%lld\n", toUnixtime(input));
+    // long long ts = toUnixtime(input);
+    // printf("%lld\n", toUnixtime(input));
+    // printf("%lld\n", ts);
+    // printf("%s\n", unixtimeToString(ts));
+    printf("%d\n", dateInRange(input,input2,input3));
     return 0;
 }
